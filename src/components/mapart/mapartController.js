6f6652c8248d0f55a70a3f772d5af9593ef0c218
Kalka <kalka2088@gmail.com>
2025-04-26 04:15:14 -0400

    Fix URLToPreset switch statement eslint error

--- src/components/mapart/mapartController.js
+++ src/components/mapart/mapartController.js
@@ -571,6 +571,8 @@ class MapartController extends Component {
         document.body.style.backgroundSize="100%";
         fetch("https://derpibooru.org/api/v1/json/search/images?q=scenery,score.gte:1000,safe&sf=random&per_page=1").then(req=>req.json()).then(derp=>document.body.style.backgroundImage=`url(${derp.images[0].representations.full})`);
         return null;
+      default:
+        break;
     }
     if (!/^[0-9a-zQ-ZA-P]*$/g.test(encodedPreset)) {
       onCorruptedPreset();
