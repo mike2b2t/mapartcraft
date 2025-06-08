cf9d1bfd7ea99f1c03233a7e8e69d79d97de3b45
mike <cypherprofessor@gmail.com>
2025-06-08 20:18:59 +0300

    Fix code style
    Replace tabs with spaces

--- src/components/mapart/workers/mapCanvas.js
+++ src/components/mapart/workers/mapCanvas.js
@@ -176,80 +176,80 @@ function squaredEuclideanMetricColours(pixel1, pixel2) {
     let cAvg = (cStd + cSmp) / 2;
 
     let cAvgPow7 = Math.pow(cAvg, 7);
-		let G =
-			0.5 *
-			(1 -
-				Math.sqrt(
-					cAvgPow7 / (cAvgPow7 + Math.pow(25, 7))
-				));
+    let G =
+      0.5 *
+      (1 -
+        Math.sqrt(
+          cAvgPow7 / (cAvgPow7 + Math.pow(25, 7))
+        ));
 
-		let apStd = aStd * (1 + G);
-		let apSmp = aSmp * (1 + G);
+    let apStd = aStd * (1 + G);
+    let apSmp = aSmp * (1 + G);
 
-		let cpStd = Math.sqrt(apStd * apStd + bStd * bStd);
-		let cpSmp = Math.sqrt(apSmp * apSmp + bSmp * bSmp);
+    let cpStd = Math.sqrt(apStd * apStd + bStd * bStd);
+    let cpSmp = Math.sqrt(apSmp * apSmp + bSmp * bSmp);
 
-		let hpStd =
-			Math.abs(apStd) + Math.abs(bStd) === 0
-				? 0
-				: Math.atan2(bStd, apStd);
-		hpStd += (hpStd < 0) * 2 * Math.PI;
+    let hpStd =
+      Math.abs(apStd) + Math.abs(bStd) === 0
+        ? 0
+        : Math.atan2(bStd, apStd);
+    hpStd += (hpStd < 0) * 2 * Math.PI;
 
-		let hpSmp =
-			Math.abs(apSmp) + Math.abs(bSmp) === 0
-				? 0
-				: Math.atan2(bSmp, apSmp);
-		hpSmp += (hpSmp < 0) * 2 * Math.PI;
+    let hpSmp =
+      Math.abs(apSmp) + Math.abs(bSmp) === 0
+        ? 0
+        : Math.atan2(bSmp, apSmp);
+    hpSmp += (hpSmp < 0) * 2 * Math.PI;
 
-		let dL = lSmp - lStd;
-		let dC = cpSmp - cpStd;
+    let dL = lSmp - lStd;
+    let dC = cpSmp - cpStd;
 
     let cpStdtimescpSmpZero = (cpStd === 0 && cpSmp === 0);
-		let dhp = cpStdtimescpSmpZero ? 0 : hpSmp - hpStd;
-		dhp -= (dhp > Math.PI) * 2 * Math.PI;
-		dhp += (dhp < -Math.PI) * 2 * Math.PI;
+    let dhp = cpStdtimescpSmpZero ? 0 : hpSmp - hpStd;
+    dhp -= (dhp > Math.PI) * 2 * Math.PI;
+    dhp += (dhp < -Math.PI) * 2 * Math.PI;
 
-		let dH = 2 * Math.sqrt(cpStd * cpSmp) * Math.sin(dhp / 2);
+    let dH = 2 * Math.sqrt(cpStd * cpSmp) * Math.sin(dhp / 2);
 
-		let Lp = (lStd + lSmp) / 2;
-		let Cp = (cpStd + cpSmp) / 2;
+    let Lp = (lStd + lSmp) / 2;
+    let Cp = (cpStd + cpSmp) / 2;
 
-		let hp;
-		if (cpStdtimescpSmpZero) {
-			hp = hpStd + hpSmp;
-		} else {
-			hp = (hpStd + hpSmp) / 2;
-			hp -= (Math.abs(hpStd - hpSmp) > Math.PI) * Math.PI;
-			hp += (hp < 0) * 2 * Math.PI;
-		}
+    let hp;
+    if (cpStdtimescpSmpZero) {
+      hp = hpStd + hpSmp;
+    } else {
+      hp = (hpStd + hpSmp) / 2;
+      hp -= (Math.abs(hpStd - hpSmp) > Math.PI) * Math.PI;
+      hp += (hp < 0) * 2 * Math.PI;
+    }
 
     let Lpminus50 = Lp - 50;
-		let Lpm50 = Lpminus50 * Lpminus50;
-		let T =
-			1 -
-			0.17 * Math.cos(hp - Math.PI / 6) +
-			0.24 * Math.cos(2 * hp) +
-			0.32 * Math.cos(3 * hp + Math.PI / 30) -
-			0.2 * Math.cos(4 * hp - (63 * Math.PI) / 180);
+    let Lpm50 = Lpminus50 * Lpminus50;
+    let T =
+      1 -
+      0.17 * Math.cos(hp - Math.PI / 6) +
+      0.24 * Math.cos(2 * hp) +
+      0.32 * Math.cos(3 * hp + Math.PI / 30) -
+      0.2 * Math.cos(4 * hp - (63 * Math.PI) / 180);
 
-		let Sl = 1 + (0.015 * Lpm50) / Math.sqrt(20 + Lpm50);
-		let Sc = 1 + 0.045 * Cp;
-		let Sh = 1 + 0.015 * Cp * T;
+    let Sl = 1 + (0.015 * Lpm50) / Math.sqrt(20 + Lpm50);
+    let Sc = 1 + 0.045 * Cp;
+    let Sh = 1 + 0.015 * Cp * T;
 
-		let deltaTheta =
-			((30 * Math.PI) / 180) *
-			Math.exp(-1 * Math.pow(((180 / Math.PI) * hp - 275) / 25, 2));
-		let Rc =
-			2 *
-			Math.sqrt(Math.pow(Cp, 7) / (Math.pow(Cp, 7) + Math.pow(25, 7)));
+    let deltaTheta =
+      ((30 * Math.PI) / 180) *
+      Math.exp(-1 * Math.pow(((180 / Math.PI) * hp - 275) / 25, 2));
+    let Rc =
+      2 *
+      Math.sqrt(Math.pow(Cp, 7) / (Math.pow(Cp, 7) + Math.pow(25, 7)));
 
-		let Rt = -1 * Math.sin(2 * deltaTheta) * Rc;
+    let Rt = -1 * Math.sin(2 * deltaTheta) * Rc;
 
     let dLdivSl = dL / Sl;
     let dCdivSc = dC / Sc;
     let dHdivSh = dH / Sh;
-		return dLdivSl * dLdivSl + dCdivSc * dCdivSc + dHdivSh * dHdivSh +
-				(((Rt * dC) / Sc) * dH) / Sh;
+    return dLdivSl * dLdivSl + dCdivSc * dCdivSc + dHdivSh * dHdivSh +
+        (((Rt * dC) / Sc) * dH) / Sh;
   }
   else {
     const r = pixel1[0] - pixel2[0];
